95dd4f3ed4e028998ddb4de5eb493cd56af4a198
Junior Monteiro <junior.monteiro@powersolution.cv>
2023-01-06 19:14:01 -0100

    Exercise2 implementation

---
+++ src/main/java/VisualNutsExer2.java
@@ -0,0 +1,148 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author : goliathhagar
+ * @CreatedBy : IntelliJ IDEA
+ * @ProjectName : VisualNuts
+ * @mailto : goliathhagar@outlook.com/thejuniormonteiro@outlook.com
+ * @created : 1/6/23, Friday, 4:57 PM
+ **/
+public class VisualNutsExer2 {
+   Set<Country> countries;
+
+    public VisualNutsExer2(Set<Country> countries) {
+        this.countries = countries;
+    }
+
+    public static class Country{
+        private final String countryCode;
+        private final Set<String> officialLanguages;
+
+        public Country(String name, Set<String> officialLanguages) {
+            this.countryCode = name;
+            this.officialLanguages = officialLanguages;
+        }
+
+        public String getCountryCode() {
+            return countryCode;
+        }
+
+        public Set<String> getOfficialLanguages() {
+            return officialLanguages;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof VisualNutsExer2.Country country)) return false;
+            return getCountryCode().equals(country.getCountryCode());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getCountryCode());
+        }
+    }
+
+    /**
+     *
+     * @return int Number of countries
+     */
+    int getTotalCountry(){
+        return countries.size();
+    }
+
+    /**
+     *
+     * @return String Country code
+     */
+    public String getGermanSpeakCountryWithMostLanguages(){
+        String contryCode = "";
+
+
+       /* Optional<Map.Entry<String, Integer>> country = countries.stream()
+                .filter(ctr -> ctr.getOfficialLanguages().contains("de"))
+                .map(ctr -> Map.entry(ctr.getCountryCode(), ctr.getOfficialLanguages().size()))
+                .max(Map.Entry.comparingByValue());*/
+
+        Optional<Map.Entry<String, Integer>> country = getCountryOfficialLanguagesQuantity("de")
+                .max(Map.Entry.comparingByValue());
+
+        if(country.isPresent())
+            contryCode = country.get().getKey();
+
+        return contryCode;
+    }
+
+    /**
+     *
+     * @return int Total Languages
+     */
+    public int getAllOfficialLAnguagues(){ //Set may be usefull
+        Set<String> languages = countries.stream()
+                .flatMap(country -> country.getOfficialLanguages().stream())
+                .collect(Collectors.toSet());
+
+        return languages.size();
+    }
+
+    /**
+     *
+     * @return String Country Code
+     */
+    public String getCountryHighestNumberOfficialLanguages(){
+        String contryCode = "";
+
+        /*Optional<Map.Entry<String, Integer>> country = countries.stream()
+                .map(ctr -> Map.entry(ctr.getCountryCode(), ctr.getOfficialLanguages().size()))
+                .max(Map.Entry.comparingByValue());*/
+        Optional<Map.Entry<String, Integer>> country = getCountryOfficialLanguagesQuantity("")
+                .max(Map.Entry.comparingByValue());
+
+        if(country.isPresent())
+            contryCode = country.get().getKey();
+
+        return contryCode;
+    }
+
+    private Stream<Map.Entry<String, Integer>> getCountryOfficialLanguagesQuantity(String countryCode ){
+        return countries.stream()
+                .filter(ctr -> ctr.getOfficialLanguages().contains(countryCode) || countryCode.isEmpty())
+                .map(ctr -> Map.entry(ctr.getCountryCode(), ctr.getOfficialLanguages().size()));
+    }
+
+    /**
+     *
+     * @return String Language Code
+     */
+    public String getMostCommonLanguage(){
+        String language = "";
+        Optional<Map.Entry<String, Long>> languages = countries.stream()
+                .flatMap(country -> country.getOfficialLanguages().stream())
+                .collect(Collectors.groupingBy(ctr -> ctr,Collectors.counting()))
+                .entrySet().stream()
+                .max(Map.Entry.comparingByValue());
+
+        if(languages.isPresent())
+            language = languages.get().getKey();
+
+        return language;
+    }
+
+    public static void main(String[] args) {
+
+        HashSet<Country> dataSource = new HashSet<>();//external api/datasource
+
+        VisualNutsExer2 exer2 = new VisualNutsExer2(dataSource);
+
+        System.out.println("Total Country: " + exer2.getTotalCountry());
+        System.out.println("Most German Speaking  Country: " + exer2.getGermanSpeakCountryWithMostLanguages());
+        System.out.println("Total Official Languages: " + exer2.getMostCommonLanguage());
+        System.out.println("Highest Number Of Official Language Country: " + exer2.getCountryHighestNumberOfficialLanguages());
+        System.out.println("Most Common Official Languages: " + exer2.getMostCommonLanguage());
+
+
+    }
+}
---
+++ src/main/java/VisualNutsExer2Alternative.java
@@ -0,0 +1,128 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @author : goliathhagar
+ * @CreatedBy : IntelliJ IDEA
+ * @ProjectName : VisualNuts
+ * @mailto : thejuniormonteiro@outlook.com
+ * @created : 1/5/23, Thursday, 6:06 PM
+ **/
+public class VisualNutsExer2Alternative {
+    public VisualNutsExer2Alternative(Map<String, List<String>> countries) {
+        this.countries = countries;
+    }
+
+    Map<String, List<String>> countries;
+    /*public class Country{
+        private final String countryCode;
+        private final Set<String> officialLanguages;
+
+        public Country(String name, Set<String> officialLanguages) {
+            this.countryCode = name;
+            this.officialLanguages = officialLanguages;
+        }
+
+        public String getCountryCode() {
+            return countryCode;
+        }
+
+        public Set<String> getOfficialLanguages() {
+            return officialLanguages;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Country country)) return false;
+            return getCountryCode().equals(country.getCountryCode());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getCountryCode());
+        }
+    }
+*/
+   /* void loadCountryData(){//load from json source
+        countries = new HashMap<>();
+
+        countries.put("US", List.of("en"));
+        countries.put("BE", List.of("nl","fr","de"));
+        countries.put("NL", List.of("nl","fy"));
+        countries.put("DE", List.of("de"));
+        countries.put("ES", List.of("es"));
+
+    }*/
+
+    int totalCountry(){
+        return countries.size();
+    }
+
+    /**
+     *
+     * @return String Country code
+     */
+    String germanSpeakCountryWithMostLanguages(){
+        String contryCode = "";
+
+        Optional<Map.Entry<String, Integer>> country = countries.entrySet().stream()
+                .filter(ctr -> ctr.getValue().contains("de"))
+                .map(ctr -> Map.entry(ctr.getKey(), ctr.getValue().size()))
+                .max(Map.Entry.comparingByValue());
+
+        if(country.isPresent())
+            contryCode = country.get().getKey();
+
+        return contryCode;
+    }
+
+    int allOfficialLAnguagues(){ //hashset may be usefull
+        Set<String> languages = countries.entrySet().stream()
+                .flatMap(country -> country.getValue().stream())
+                .collect(Collectors.toSet());
+
+        return languages.size();
+    }
+
+    String countryHighestNumberOfficialLanguages(){
+        String contryCode = "";
+
+        Optional<Map.Entry<String, Integer>> country = countries.entrySet().stream()
+                .map(ctr -> Map.entry(ctr.getKey(), ctr.getValue().size()))
+                .max(Map.Entry.comparingByValue());
+
+        if(country.isPresent())
+            contryCode = country.get().getKey();
+
+        return contryCode;
+    }
+
+    String mostCommonLanguage(){
+        String language = "";
+
+        Optional<Map.Entry<String, Long>> languages = countries.entrySet().stream()
+                .flatMap(country -> country.getValue().stream())
+                .collect(Collectors.groupingBy(ctr -> ctr,Collectors.counting()))
+                .entrySet().stream()
+                .max(Map.Entry.comparingByValue());
+
+        if(languages.isPresent())
+            language = languages.get().getKey();
+
+        return language;
+    }
+
+    public static void main(String[] args) {
+        VisualNutsExer2Alternative exer2 = new VisualNutsExer2Alternative(Map.of());
+
+
+        System.out.println("Total Country: " + exer2.totalCountry());
+        System.out.println("Most German Speaking  Country: " + exer2.germanSpeakCountryWithMostLanguages());
+        System.out.println("Total Official Languages: " + exer2.allOfficialLAnguagues());
+        System.out.println("Highest Number Of Official Language Country: " + exer2.countryHighestNumberOfficialLanguages());
+        System.out.println("Most Common Official Languages: " + exer2.mostCommonLanguage());
+
+
+    }
+}
